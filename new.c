a88237da81d45a37da9cb366c98a1be64017174a
Aryan Jaiswal <aryanjaiswal166@gmail.com>
2021-06-28 17:54:54 +0530

    first commit

---
+++ new.c
@@ -0,0 +1,16 @@
+/**
+ * @file new.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-06-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
